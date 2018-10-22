0f62a82b60dc07ecefa977946be449f8b5f4c3ab
Reka <reekaa85@gmail.com>
2018-10-22 18:31:52 +0200

    thymeleaf added to project

--- src/main/java/org/renting/rentanrv/controller/WelcomeController.java
+++ src/main/java/org/renting/rentanrv/controller/WelcomeController.java
@@ -1,13 +1,12 @@
 package org.renting.rentanrv.controller;
 
-import javax.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import org.springframework.stereotype.Component;
-
-@Component
+@Controller
 public class WelcomeController {
-	@PostConstruct
-	public void welcome() {
-		System.out.println ("welcome");
+	@GetMapping(value = {"/", "/rvs"})
+	public String welcome() {
+		return ("index");
 	}
 }
