e79cbcdc6d88c63730c823b25b10704ba374a466
Reka <reekaa85@gmail.com>
2018-10-23 17:34:27 +0200

    vehicle model added

---
+++ src/main/java/org/renting/rentanrv/model/Vehicle.java
@@ -0,0 +1,129 @@
+package org.renting.rentanrv.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Vehicle {
+	// -- constructors --
+	
+		public Vehicle() {}
+
+		public Vehicle(String name, int numberOfGuests, String localisation, int numberOfBeds,
+				BigDecimal pricePerNight, int minStay) {
+			this.name = name;
+			this.numberOfGuests = numberOfGuests;
+			this.localisation = localisation;
+			this.numberOfBeds = numberOfBeds;
+			this.pricePerNight = pricePerNight;
+			this.minStay = minStay;
+		}
+	
+	// -- fields --
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@NotNull
+	@Size(min=3, max=40)
+	private String name;
+	
+	@NotNull
+	@Min(1)
+	@Max(20)
+	private int numberOfGuests;
+	
+	@NotNull
+	@Size(min=5, max=80)
+	private String localisation;
+	
+	//kind
+	
+	@NotNull
+	@Min(1)
+	@Max(20)
+	private int numberOfBeds;
+	
+	@NotNull
+	@Min(0)
+	private BigDecimal pricePerNight;
+	
+	@Min(1)
+	private int minStay;
+	
+	
+	
+	// -- getters, setters, toString --
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumberOfGuests() {
+		return numberOfGuests;
+	}
+
+	public void setNumberOfGuests(int numberOfGuests) {
+		this.numberOfGuests = numberOfGuests;
+	}
+
+	public String getLocalisation() {
+		return localisation;
+	}
+
+	public void setLocalisation(String localisation) {
+		this.localisation = localisation;
+	}
+
+	public int getNumberOfBeds() {
+		return numberOfBeds;
+	}
+
+	public void setNumberOfBeds(int numberOfBeds) {
+		this.numberOfBeds = numberOfBeds;
+	}
+
+	public BigDecimal getPricePerNight() {
+		return pricePerNight;
+	}
+
+	public void setPricePerNight(BigDecimal pricePerNight) {
+		this.pricePerNight = pricePerNight;
+	}
+
+	public int getMinStay() {
+		return minStay;
+	}
+
+	public void setMinStay(int minStay) {
+		this.minStay = minStay;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [name=" + name + ", NumberOfGuests=" + numberOfGuests + ", localisation=" + localisation
+				+ ", NumberOfBeds=" + numberOfBeds + ", PricePerNight=" + pricePerNight + ", MinStay=" + minStay + "]";
+	}
+	
+}
